9aedf7576ca6a0609947d02227c089a1cf604535
Xin Shi <Xin.Shi@outlook.com>
2022-01-27 17:02:55 +0800

    update setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="raser",
-    version="2.2.1",
+    version="3.1.0",
     author="Xin Shi",
     author_email="Xin.Shi@outlook.com",
     description="SiC Detector Simulation",
